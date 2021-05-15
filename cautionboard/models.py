5a1f5c9f11125a643d42ccf01dd196291c49efc2
Bae-jihyun <govl6113@naver.com>
2021-05-15 20:51:45 +0900

    views, model변경

--- cautionboard/models.py
+++ cautionboard/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-class Comment(models.Model):
+class Comment(models.Model):   #사용자들이 입력한 댓글(정보). 주의사항 보여줌
     author = models.CharField(max_length=5)
-    place = models.CharField(max_length=10) 
+    place = models.CharField(max_length=10,foreign_key=True) 
     caution = models.TextField()
-    id=models.IntegerField(primary_key=True)
     pub_date = models.DateTimeField()
     no=models.IntegerField()
     yes= models.IntegerField()
@@ -13,7 +12,10 @@ class Comment(models.Model):
 
 
 
-class Text(models.Model):
-    place = models.CharField(max_length=10)
-    plaInt= models.TextField()
+class Text(models.Model):  #개발자가 입력하는 정보
+    place = models.CharField(max_length=10,primary_key=True)
+    placeInformation= models.TextField()
 
+
+    def __str__(self) :
+        return self.place
--- cautionboard/views.py
+++ cautionboard/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from.models import Comment
+from.models import Text
+from django.utils import timezone
+from PerfecTrip import cautionboard
 
 # Create your views here.
 def home(request):
@@ -6,3 +10,16 @@ def home(request):
 
 def detail(request):
     return render(request, "detail.html")
+
+def addcaution(request):
+    comment=Comment()
+    comment.author=request.POST.get('author',False)
+    comment.place=request.POST.get('place',False)
+    comment.caution=request.POST.get('caution',False)
+    comment.id=request.POST.get('id',False)
+    comment.no=request.POST.get('no',False)
+    comment.yes=request.POST.get('yes',False)
+    comment.pet=request.POST.get('pet',False)
+    comment.tripType=request.POST.get('tripType',False)
+    comment.pub_date=timezone.datetime.now()
+    comment.save()
